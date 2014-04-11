c2b8c30fd8b7361f0379ca02ae5ac8d4f4a81283
Michael <mihai.mandrescu@gmail.com>
2014-04-11 13:18:42 +0300

    BF for ProLog - Not my implementation.

---
+++ bfPl.pl
@@ -0,0 +1,34 @@
+/*
+cod preluat din cartea(bibliografie[1]):
+BALCAN Maria Florina, HRISTEA Florentina, 
+Aspecte ale Cautarii si Reprezentarii Cunostintelor in Inteligenta Artificiala,
+Editura Universitatii din Bucuresti, 2004
+*/
+
+
+%concat(+Lista1,+Lista2,-Listarez)
+concat([],L,L).
+concat([H|T],L,[H|T1]):-concat(T,L,T1).
+
+%membru(+Element,+Lista)
+membru(H,[H|_]).
+membru(X,[_|T]):-membru(X,T).
+
+%rezolva_b(+Start,- Sol)
+rezolva_b(Start, Sol):-breadthfirst([[Start]],Sol).
+
+%breadthfirst(+Listadrumuri,-DrumSolutie)
+breadthfirst([[Nod|Drum]|_], [Nod|Drum]):- scop(Nod).
+breadthfirst([Drum|Drumuri], Sol) :- 
+				extinde(Drum, DrumuriNoi),
+				concat(Drumuri, DrumuriNoi, Drumuri1),
+				breadthfirst(Drumuri1, Sol).
+
+%extinde(+StareDrum,-ListaDrumuriDerivate)
+extinde([Nod|Drum],DrumuriNoi):-
+				bagof([NodNou,Nod|Drum], (s(Nod,NodNou), \+(membru(NodNou,[Nod|Drum]))),
+				DrumuriNoi),
+				!.
+extinde(_,[]).
+	
+
