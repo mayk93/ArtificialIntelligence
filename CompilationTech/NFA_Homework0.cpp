d9e8ac937eb8ce21ec1864fa515e0e93e0c431c2
michael <michael@michael-PC.(none)>
2015-02-21 00:55:07 +0200

    NFA Assignment Compilation Tech, Added Label

--- CompilationTech/NFA_Homework0.cpp
+++ CompilationTech/NFA_Homework0.cpp
@@ -7,12 +7,18 @@ int main(int argc, char* argv[])
     
     //Widgets
     GtkWidget* window;
+    GtkWidget* label;
     
-    //Widgets Functionality
+    //Widgets Functionality:
+    
+        //Initialize Widgets
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    label = gtk_label_new("Nondeterministic Finite Automaton");
+        //Functionality
     g_signal_connect(window,"delete-event",G_CALLBACK(gtk_main_quit),NULL);
     gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
-    gtk_widget_show(window);
+    gtk_container_add(GTK_CONTAINER(window),label);
+    gtk_widget_show_all(window);
     
     //Run
     gtk_main();
