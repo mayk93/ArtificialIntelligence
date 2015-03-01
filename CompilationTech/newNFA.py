6dffb4fe3cfd697a073a54a755f1fd19ef2fc244
mayk93 <mihai.mandrescu@gmail.com>
2015-03-01 23:51:39 +0200

    Implemented NFA draft
    Most implementation is done, inputs and other testing required

--- CompilationTech/newNFA.py
+++ CompilationTech/newNFA.py
@@ -17,9 +17,50 @@ transitionWithA[possibleNextState] = 2
 '''
 possibleCurrentState = 0
 possibleNextState = 1
+startSymbolIndex = 0
+initialState = 1
 
-def nfa():
-    return 0
+reachedFinalState = False
+
+def nfa(inputString,currentSymbolIndex,currentState):
+    '''
+    Here, I check if I entered an empty string.
+    If I did, I must see if there are any lambda transitions from the initial state
+    '''
+    if len(inputString) == 0:
+        currentPossibleLambdaTransitions = possibleLambdaTransitions(currentState)
+        if len(currentPossibleLambdaTransitions) > 0:
+            for transition in currentPossibleLambdaTransitions:
+                nfa(inputString,currentSymbolIndex,transition)
+        else:
+            if currentState in finalStates:
+                global reachedFinalState
+                reachedFinalState = True
+                return
+            else:
+                return
+    '''
+    Here, I still have symbols to check
+    '''
+    if currentSymbolIndex < len(inputString) and reachedFinalState == False:  
+        for transition in currentPossibleTransitions:
+            nfa(inputString,newSymbolIndex,transition)
+    else:
+        '''
+        Here, I run out of symbols to parse, so I check if I am in a final state,
+        or if I can make lambda transitions to get to a final state
+        '''
+        currentPossibleLambdaTransitions = possibleLambdaTransitions(currentState)
+        if len(currentPossibleLambdaTransitions) > 0:
+            for transition in currentPossibleLambdaTransitions:
+                nfa(inputString,currentSymbolIndex,transition)
+        else:
+            if currentState in finalStates:
+                global reachedFinalState
+                reachedFinalState = True
+                return
+            else:
+                return
 
 def possibleTransitions(currentSymbol,currentState):
     possibleTransitions = []
@@ -33,8 +74,16 @@ def possibleTransitions(currentSymbol,currentState):
             possibleTransitions.append(transition[possibleNextState])
     return list(set(possibleTransitions))
 
+def possibleLambdaTransitions(currentState):
+    possibleLambdaTransitions = []
+    for currentLambdaTransition in delta[lambdaTransition]:
+        if currentState == currentLambdaTransition[possibleCurrentState]:
+            possibleLambdaTransitions.append(currentLambdaTransition[possibleNextState])
+    return list(set(possibleLambdaTransitions))
+
 def main():
-    nfa(inputString)
+    nfa(inputString,startSymbolIndex,initialState)
+    print(reachedFinalState)
 
 if __name__ == "__main__":
     main()
