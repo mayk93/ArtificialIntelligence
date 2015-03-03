1ad4ac517878f4e972790525d85c8bcba53c2422
mayk93 <mihai.mandrescu@gmail.com>
2015-03-04 00:45:36 +0200

    Updated Bisection Method
    Updated Bisection Method

--- NumericalCalculus/metoda_bisectiei.py
+++ NumericalCalculus/metoda_bisectiei.py
@@ -1,45 +1,60 @@
+#Libraries
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-
-solutii = []
-
-def f (x):
-    return 1.0/2.0 + 1.0/4.0 * x**2 - x * math.sin(x) + 1.0/2.0 * math.cos(2 * x)
-    #return x**3 +  4.0 * x**2 - 10.0
-    #return math.sqrt(x**2 - 1)
-
-
-
-def metoda_bisectiei (a, b, err, n):
-    
-    c = (a + b) / 2.0
-    fa = f(a)
-    fb = f(b)
-    fc = f(c)
-    if (fc == 0 or math.fabs(fc) < err):
-        print ("c = " + str(c))
-        print ("n:" + str(n))
-        solutii.append(c)
+#Root parameters
+roots = []
+numberOfRoots = 5
+precision = -13
+#Function Plot
+start = 0
+end = 10
+#Axis Plot
+plotXStart = -10
+plotXEnd = 10
+plotYStart = -10
+plotYEnd = 10
+#Auxiliary mathematical functions
+e = math.e
+def sin(x):
+    return math.sin(x)
+def cos(x):
+    return math.cos(x)
+#Function to analyze
+def f(x):
+    return 3 - e**(sin(2*(x**2)) + cos(x**2))
+#The Bisection Method
+def bisectionMethod (a, b, err, n):
+    if len(roots) < numberOfRoots:
+        c = (a + b) / 2.0
+        fa = f(a)
+        fb = f(b)
+        fc = f(c)
+        if fc == 0 or math.fabs(fc) < err:
+            print ("c = " + str(c))
+            print ("n:" + str(n))
+            roots.append(c)
+            return
+        elif (fa * fc < 0):
+            bisectionMethod(a, c, err, n + 1)
+        elif (fc * fb < 0):
+            bisectionMethod(c, b, err, n + 1)
+    else:
         return
-    elif (fa * fc < 0):
-        metoda_bisectiei(a, c, err, n + 1)
-    elif (fc * fb < 0):
-        metoda_bisectiei(c, b, err, n + 1)
-
-
+#Main and Plot
 if __name__ == "__main__":
     
-    for i in np.arange(-10, 10, 0.1):
-        metoda_bisectiei(i, i + 0.1, 10.0**-10, 0)
+    for i in np.arange(start, end, 0.1):
+        bisectionMethod(i, i + 0.1, 10.0**precision, 0)
         
-    t = np.arange(-10, 10, 0.1)
+    t = np.arange(start, end, 0.1)
     t1 = [ f(x) for x in t]
+    
     plt.plot(t, t1, '-b')
-    plt.plot([-10, 10], [0, 0], '-k')
-    plt.plot([0, 0], [-10, 10], '-k')
-    plt.plot(solutii, [0 for x in range(len(solutii))], 'or')
-    plt.axis([-10, 10, -10, 10])
-    plt.title('Metoda bisectiei')
+    plt.plot([plotXStart, plotXEnd], [0, 0], '-k')
+    plt.plot([0, 0], [plotYStart, plotYEnd], '-k')
+    plt.plot(roots, [0 for x in range(len(roots))], 'or')
+    plt.axis([plotXStart, plotXEnd, plotYStart, plotYEnd])
+    plt.title('Bisection Method')
     plt.show()   
     
