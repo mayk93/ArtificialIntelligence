38df784afa5449f75af266eb4efccd5dbd58c45b
mayk93 <mihai.mandrescu@gmail.com>
2015-03-21 19:10:34 +0200

    NLP Tutorial
    Set up data base.

---
+++ License/Test/sentimentAnalisys.py
@@ -0,0 +1,20 @@
+#Refactor: Rewrite with MongoDB
+
+#Libraries
+import sqlite3
+
+"""
+Data Base Initialization
+"""
+'''
+#Variables
+connection = sqlite3.connect("knowledgeBase.db")
+cursor = connection.cursor()
+#Functions
+def createDB():
+    cursor.execute()
+'''
+
+"""
+Sentiment Analisys
+"""
