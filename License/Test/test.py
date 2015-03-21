2f5bc19ffbf882f654ae53698504ca769f9a976e
mayk93 <mihai.mandrescu@gmail.com>
2015-03-21 18:58:48 +0200

    NLP Tutorial
    Named Entity recognition.

--- License/Test/test.py
+++ License/Test/test.py
@@ -1,18 +1,41 @@
+#Libraries
 import nltk
 import re
 import time
 
-exampleArray = ["The incredibly intimidating NLP scares people away who are sissies."]
-
+#Explanations
 """
 RegEx:
 ? : 0 or 1 repetitions
 * : 0 or more repetitions
 + : 1 ore more repetitions
-. : any character except for noow line
+. : any character except for new line
 \w: any letter
+| : or
+Chunk:
+A regex that groups by part of speech.
+
+<RB\w?>*<VB\w?>*<NNP>
+Here, I am saying I want an adverb, the RB.
+However, I also accept adverb subtypes (RBR,RBS), hence RB\w?.
+The regex RB\w? means RB followed by none or one letter.
+The same for VB ( verb ) and NNP (singular proper noun)
+
+{<.*>}
+}<RB|NNS>{
+This will chunk everything (.*), then remove
+every adverb and plural noun.
+
+namedEnt = nltk.ne_chunk(tagged,binary=True)
+In recognizing named entities, the parameter "binary=True" means:
+The binary flag is set to True to indicate only whether a subtree is NE or not.
+When set to False it will give more information
+like whether the NE is an Organization, Person etc.
+See: http://stackoverflow.com/questions/26862970/nltk-ner-word-extraction
 """
 
+#Assets
+exampleArray = ["The incredibly intimidating NLP scares people away who are sissies."]
 contentArray =['Starbucks is doing very well lately.',
                'Overall, while it may seem there is already a Starbucks on every corner, Starbucks still has a lot of room to grow.',
                'They just began expansion into food products, which has been going quite well so far for them.',
@@ -25,27 +48,22 @@ contentArray =['Starbucks is doing very well lately.',
                'Given the dry weather, coffee farmers have amped up production, to take as much of an advantage as possible with the dry weather.',
                'Increase in supply... well you know the rules...',]
 
+#Methods
 def processLanguage():
     try:
          for item in exampleArray:
              tokenized = nltk.word_tokenize(item)
-             tagged = nltk.pos_tag(tokenized) #Pos as in part of speech
-             print(tagged)
+             tagged = nltk.pos_tag(tokenized)
 
-             # Here, I'm telling it I want an adverb (RB)
-             # but I also accept adverb subtypes,
-             # RB\w? means RB followed by none or one letter
-             chunkGram = r"""Chunk: {<RB\w?>*<VB\w?>*<NNP>}""" #Regex care grupeaza pe POS-uri
-
-             chunkParser = nltk.RegexpParser(chunkGram)
-
-             chunked = chunkParser.parse(tagged)
-
-             print(chunked)
-
-             chunked.draw()
+             namedEnt = nltk.ne_chunk(tagged,binary=True)
+             namedEnt.draw()
 
     except Exception as e:
         print(str(e))
 
-processLanguage()
+#Main
+def main():
+    processLanguage()
+
+if __name__=="__main__":
+    main()
