3b1e30e458a82159c1b0f3752c54a30780155630
mayk93 <mihai.mandrescu@gmail.com>
2015-04-21 23:00:49 +0300

    Conflict Resolution

--- License/Progress/Saturday-11042015/newRecorder.py
+++ License/Progress/Saturday-11042015/newRecorder.py
@@ -3,6 +3,10 @@
 #!/usr/bin/env python
 from ctypes import *
 import pyaudio
+import socket
+import sys
+import time
+import os
 
 # From alsa-lib Git 3fd4ab9be0db7c7430ebd258f2717a976381715d
 # $ grep -rn snd_lib_error_handler_t
@@ -27,13 +31,30 @@ asound.snd_lib_error_set_handler(None)
 p = pyaudio.PyAudio()
 p.terminate()
 
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print('Created socket')
+except socket.error:
+    print('Failed to create socket')
+    sys.exit()
+ 
+host = '104.155.13.116';
+port = 5555;
+
 import speech_recognition as sr
 r = sr.Recognizer()
 r.energy_threshold = 150
+print("Listening:")
 with sr.Microphone() as source:                # use the default microphone as the audio source
     audio = r.listen(source)                   # listen for the first phrase and extract it into audio data
-
+    s.sendto(audio, (host, port))
+'''
 try:
-    print("You said " + r.recognize(audio))    # recognize speech using Google Speech Recognition
+    print("You said: " + r.recognize(audio))    # recognize speech using Google Speech Recognition
 except LookupError:                            # speech is unintelligible
     print("Could not understand audio")
+'''
+d = s.recvfrom(1024)
+reply = d[0]
+addr = d[1]
+print('Server reply : ' + reply.decode('utf-8'))
